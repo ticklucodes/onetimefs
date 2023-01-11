b730ac087e34ed5b7c254e69408aefd16229ec77
ticklu1991 <ticklu.codes@gmail.com>
2023-01-11 23:30:57 +0530

    first commit

---
+++ src/Contact.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+
+const Contact = () => {
+  const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+
+    .container {
+      margin-top: 6rem;
+      text-align: center;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  return (
+    <Wrapper>
+      <h2 className="common-heading">Feel Free to Contact us</h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.2226348984054!2d77.7130312!3d13.0214897!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae104da55c49a9%3A0xbed4efeab2142b49!2s6%2C%201st%20Cross%20Rd%2C%20Banashankari%20Layout%2C%20Hosabasavanapura%2C%20Krishnarajapura%2C%20Bengaluru%2C%20Karnataka%20560049!5e0!3m2!1sen!2sin!4v1673446366945!5m2!1sen!2sin"
+        width="100%"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+
+
+
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xgedgjkr"
+            method="POST"
+            className="contact-inputs">
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              autoComplete="off"
+              required
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="message"
+              cols="30"
+              rows="6"
+              autoComplete="off"
+              required></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Contact;
